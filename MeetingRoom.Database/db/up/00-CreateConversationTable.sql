ea947c80c59498415318bb447606e2472aa4f886
jonfraser <fraser.jc@gmail.com>
2015-05-23 22:57:35 +1000

    added roundhouse and got 2 required tables into database

---
+++ MeetingRoom.Database/db/up/00-CreateConversationTable.sql
@@ -0,0 +1,12 @@
+﻿create table Conversation
+(
+	ID int identity(1,1) not null,
+	RoomID int not null,
+	Permalink uniqueidentifier not null,
+	UserCreated nvarchar(200) not null,
+	TimeCreated datetime not null,
+	Message nvarchar(max) not null,
+	PRIMARY KEY CLUSTERED 
+(
+	ID ASC
+))
---
+++ MeetingRoom.Database/db/up/00-CreateRoomTable.sql
@@ -0,0 +1,10 @@
+﻿create table Rooms
+(
+	ID int identity(1,1) not null,
+	Name nvarchar(200) not null,
+	Permalink uniqueidentifier not null,
+	UserCreated nvarchar(200) not null,
+	PRIMARY KEY CLUSTERED 
+(
+	ID ASC
+))
